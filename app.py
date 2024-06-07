c42b05dabf2016e91678ce71460361289c35b1b1
Adida <adida.barack@student.moringaschool.com>
2024-06-07 21:19:32 +0300

    New CLI app

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-# pyenv
-# PIP installs packages
-# pypi npmjs
-# pipenv
+#!/usr/bin/env python3
+from calculations import sum
 
-def def_user_input():
-    pass
+def get_user_input():
+    while True:
+        print("""
+            Here are your options:
+                1. Create a todo
+                2. Mark a todo as done
+                3. Delete a todo
+                4. View all todos
+                5. Find a todo by name
+                6. Exit app
+            """)
+        option = input("Enter your option: ")
+        if option == "1":
+            pass # call a function to create the todo
+        if option == "6":
+            break
+
+if __name__ == "__main__":
+    get_user_input()
